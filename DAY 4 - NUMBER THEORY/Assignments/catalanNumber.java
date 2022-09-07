123a61a97216cce56159ecbbf575a249ba5a4974
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-07 19:06:24 +0530

    Create catalanNumber.java

---
+++ DAY 4 - NUMBER THEORY/Assignments/catalanNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] catalan = new int[n+1];
+		catalan[0] = 1;
+		catalan[1] = 1;
+		for(int i=2;i<=n;i++)
+		{
+		    catalan[i]=0;
+		    for(int j=0;j<i;j++)
+		    {
+		        catalan[i] += catalan[j] * catalan [i-j-1];
+		    }
+		}
+		System.out.println(catalan[n]);
+	}
+}
