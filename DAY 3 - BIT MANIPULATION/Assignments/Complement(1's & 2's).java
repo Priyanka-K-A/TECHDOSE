08817774fcd789480fd87d90d959b768ba48705a
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-07 11:52:17 +0530

    Delete Complement(1's & 2's).java

--- DAY 3 - BIT MANIPULATION/Assignments/Complement(1's & 2's).java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		System.out.println("1's Complement : "+(~n)+"\n2's Complement : "+((~n)+1));
-	}
-}
