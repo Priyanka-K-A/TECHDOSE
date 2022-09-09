c79ba0906e5bacac525e0d5d70bf8ebfa26d1f79
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-09 18:06:02 +0530

    Update 9.MeanMedianMode.java

--- BEGINNERS ASSIGNMENT/ASSIGNMENT 1/9.MeanMedianMode.java
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/9.MeanMedianMode.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Main
 {
     public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0;i<n;i++)
         {
@@ -17,7 +17,11 @@ public class Main
         }
         //Median
         Arrays.sort(arr);
-        int median = (arr[n/2]+arr[n/2-1])/2;
+        int median;
+        if(n%2==0)
+            median = (arr[n/2]+arr[n/2-1])/2;
+        else
+            median = arr[n/2]/2;
         //Mode
         int count=0,maxCount=0,val=0;
         for(int i=0;i<n;i++)
@@ -37,12 +41,11 @@ public class Main
             }
         }
         System.out.println("Mean : "+mean/n+"\n"+"Median : "+median+"\n"+"Mode : "+val);
-    
     }
 }
 //Output
 // 6
-// 1 1 2 2 3 3 
+// 1 2 3 4 5 1
 // Mean : 2
 // Median : 2
 // Mode : 1
