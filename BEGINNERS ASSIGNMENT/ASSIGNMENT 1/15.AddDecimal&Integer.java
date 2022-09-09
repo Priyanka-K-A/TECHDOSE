32bdc002462ed56ce996951854de2ec30be9b4d2
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-09 17:56:24 +0530

    Create 15.AddDecimal&Integer.java

---
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/15.AddDecimal&Integer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		float f = sc.nextFloat();
+		System.out.println((float)n+f);
+    
+    }
+}
