14f85c378f0e9a47b68dcf7f2731beec7e4be094
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-09 12:31:55 +0530

    Create 2.GcdOfNumbers.java

---
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/2.GcdOfNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+    public static int gcd(int n1,int n2)
+    {
+        return (n1==0)?n2:gcd(n2%n1,n1);
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		System.out.println(gcd(n1,n2));
+	}
+}
