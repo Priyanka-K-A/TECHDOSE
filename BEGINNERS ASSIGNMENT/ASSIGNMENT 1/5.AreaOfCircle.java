aaaf0417a8723d12083df53a54474a3aa97b8041
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-09 14:49:47 +0530

    Create 5.AreaOfCircle.java

---
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/5.AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		System.out.println(Math.PI*r*r);
+		System.out.format("%.2f",Math.PI*r*r);//upto 2 decimal places
+        
+    }
+}
