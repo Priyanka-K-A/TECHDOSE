00903a175144fe980244eeb5646fe56bc0570374
Priyanka-K-A <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-10 12:30:53 +0530

    Create InsertionSort.java

---
+++ SORTING/InsertionSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int[] arr = new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        arr[i] = sc.nextInt();
+	    }
+	    List<Integer> list = new ArrayList<Integer>();
+	    for(int i=0 ;i<n;i++)
+	    {
+	        list.add(arr[i]);
+	        for(int j=list.size()-1;j>0;j--)
+	        {
+	            if(list.get(j)>=list.get(j-1))
+	            {
+	                break;
+	            }
+	            Collections.swap(list,j,j-1);
+	        }
+	    }
+	    for(int i:list)
+	    {
+	        System.out.print(i+" ");
+	    }
+	    
+	}
+}
