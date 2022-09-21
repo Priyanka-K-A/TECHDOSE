6f2d33cd3c0740bbb0f31369061f81ca4996e586
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-21 07:09:40 +0530

    Update pascals'sTriangle(nCr).java

--- MATH ALGORITHMS/Assignments/pascals'sTriangle(nCr).java
+++ MATH ALGORITHMS/Assignments/pascals'sTriangle(nCr).java
@@ -4,7 +4,7 @@ public class Main
 	public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int size = sc.nextInt();
-    	int[][] mat = new int[size][size];
+    	int[][] mat = new int[size+1][size+1];
     	for(int i=0;i<=size;i++)
     	{
     	    for(int j=0;j<=i;j++)
