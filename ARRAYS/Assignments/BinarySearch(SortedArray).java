501e3d77d50d2e470667fe82ecccf0e880a32c95
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-09-22 15:04:14 +0530

    Create BinarySearch(SortedArray).java

---
+++ ARRAYS/Assignments/BinarySearch(SortedArray).java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i] = sc.nextInt();
+		}
+		int target = sc.nextInt();
+		int ans=0;
+		int low=0,high=n-1;
+		while(low<=high)
+		{
+		    int mid = low+(high-low)/2;
+		    if(target == arr[mid])
+		    {
+		        ans = mid;
+		        break;
+		    }
+		    else if(target>arr[mid])
+		    {
+		        low = mid+1;
+		    }
+		    else
+		    {
+		       high = mid-1;
+		    }
+		}
+		System.out.println(ans);
+	}
+}
