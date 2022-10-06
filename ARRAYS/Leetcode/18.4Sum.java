e30a498e7fbca6b412349cc21e67993d8b451e6a
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-06 14:24:53 +0530

    Create 18.4Sum.java

---
+++ ARRAYS/Leetcode/18.4Sum.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> list  = new ArrayList<>();
+        int len = nums.length;
+        if(len<4)
+            return list;
+        for(int i=0;i<=len-4;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            for(int j=i+1;j<=len-3;j++)
+            {
+                if(j>i+1 && nums[j]==nums[j-1])
+                {
+                    continue;
+                }
+                int left = j+1,right = len-1;
+                while(left<right)
+                {
+                    long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target)
+                    {
+                        List<Integer> newList = new ArrayList<>();
+                        newList.add(nums[i]);
+                        newList.add(nums[j]);
+                        newList.add(nums[left]);
+                        newList.add(nums[right]);
+                        if(!list.contains(newList))
+                        {
+                            list.add(newList);
+                        }
+                        left++;
+                    }
+                    else if(sum>target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                } 
+            }
+        }
+        return list;
+    }
+}
