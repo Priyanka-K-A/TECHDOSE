a6e85183fe727e04a95ac30668b1fced5762a90e
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-11 15:33:35 +0530

    Create WeeklyContest_301.java

---
+++ LEETCODE CONTESTS SOLVED/WeeklyContest_301.java
@@ -0,0 +1,10 @@
+//2335. Minimum Amount of Time to Fill Cups
+class Solution {
+    public int fillCups(int[] amount) {
+        Arrays.sort(amount);
+        if(amount[0]+amount[1]>amount[2])
+            return ((amount[0]+amount[1]+amount[2])/2)+((amount[0]+amount[1]+amount[2])%2);
+        else
+            return amount[2];
+    }
+}
