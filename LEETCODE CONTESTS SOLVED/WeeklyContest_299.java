a20de0bd26969875b88c0f51dca036a4a9af07ca
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-11 22:26:36 +0530

    Create WeeklyContest_299.java

---
+++ LEETCODE CONTESTS SOLVED/WeeklyContest_299.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int flag=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(i==j||j==row-i-1)
+                {
+                    if(grid[i][j]==0)
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+                else
+                {
+                    if(grid[i][j]!=0)
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+            }
+        }
+        return (flag==0)?true:false;
+        
+    }
+}
