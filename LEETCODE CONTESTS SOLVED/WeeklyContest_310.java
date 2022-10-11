9c5145212c4144dda5b0c38e6adcda6719c00e7b
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-11 09:31:15 +0530

    Create WeeklyContest_310.java

---
+++ LEETCODE CONTESTS SOLVED/WeeklyContest_310.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        int[] count = new int[100001];
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%2==0)
+            {
+                count[nums[i]]++;
+            }
+        }
+        int max=0;
+        for(int i=0;i<100001;i++)
+        {
+            if(count[i]>max)
+            {
+                max = count[i];
+                k = i;
+            }
+        }
+        if(max==0)
+        {
+            return -1;
+        }
+        return k;
+    }
+}
