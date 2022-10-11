dd2492b9cc0363b271d049faa770eeeea6996b90
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-11 09:33:30 +0530

    Create WeeklyContest_309.java

---
+++ LEETCODE CONTESTS SOLVED/WeeklyContest_309.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int k=0,s1=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            for(int j=i+1;j<s.length();j++)
+            {
+                if(s.charAt(i)==s.charAt(j))
+                {
+                    k = (int)s.charAt(i);
+                    if((j-i-1)!=distance[k-97])
+                    {
+                        s1=1;
+                        break;
+                    }
+                }
+            }
+        }
+        return (s1==1)?false:true;
+    }
+}
