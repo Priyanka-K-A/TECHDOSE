abbe23270119be3c15174dce64ab9c385d6c49dc
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-14 10:54:13 +0530

    2239. Find Closest Number to Zero

---
+++ LEETCODE CONTESTS SOLVED/BiWeeklyContest_76.java
@@ -0,0 +1,20 @@
+//2239. Find Closest Number to Zero
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int closest = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(Math.abs(nums[i])<min)
+            {
+                min = Math.abs(nums[i]);
+                closest = nums[i];
+            }
+            else if(Math.abs(nums[i])==min)
+            {
+                closest = Math.max(nums[i],closest);
+            }
+        }
+        return closest;
+    }
+}
