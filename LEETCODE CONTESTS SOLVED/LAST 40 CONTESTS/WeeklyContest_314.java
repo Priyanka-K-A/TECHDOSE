70ef4b32c41e9705177d986040f9f9033f578e3e
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-19 09:36:57 +0530

    2433. Find The Original Array of Prefix Xor

---
+++ LEETCODE CONTESTS SOLVED/LAST 40 CONTESTS/WeeklyContest_314.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+        int len = pref.length;
+        int[] arr = new int[len];
+        for(int i=0;i<len;i++)
+        {
+            if(i==0)
+            {
+                arr[i] = pref[i];
+            }
+            else
+            {
+                arr[i] = pref[i-1]^pref[i];
+            }
+        }
+        return arr;
+    }
+}
