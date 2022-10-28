03bef768f6339af67fbe7ca7bc89efb3f4b7caac
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-28 12:06:52 +0530

    Create Implementation of Queue using Linked List.java

---
+++ STACKS AND QUEUES/Implementation/Implementation of Queue using Linked List.java
@@ -0,0 +1,66 @@
+class Node 
+{
+    int data;
+    Node next;
+}
+class QueueUsingLL
+{
+    Node front = null,rear=null;
+    Node createNode(int data)
+    {
+        Node nn = new Node();
+        nn.data = data;
+        nn.next = null;
+        return nn;
+    }
+    void enqueue(int data)
+    {
+        Node nn = createNode(data);
+        if(front==null)
+        {
+            rear = nn;
+            front = nn;
+        }
+        else
+        {
+            Node temp = front;
+            while(temp.next!=null)
+            {
+                temp = temp.next;
+            }
+            temp.next = nn;
+            rear = nn;
+        }
+    }
+    void deque()
+    {
+        if(front!=null)
+        {
+            front = front.next;
+        }
+    }
+    void display()
+    {
+        Node temp = front;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+public class Main 
+{
+    public static void main (String[] args) 
+    {
+        QueueUsingLL qll = new QueueUsingLL();
+        qll.enqueue(10);
+        qll.enqueue(20);
+        qll.enqueue(30);
+        qll.enqueue(40);
+        qll.display();
+        qll.deque();
+        qll.display();
+    }
+}
