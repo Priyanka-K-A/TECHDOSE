0dd301f7bda273c90cbf838f8c74ea7d9a707f1a
PRIYANKA ASHOKAN <108280855+Priyanka-K-A@users.noreply.github.com>
2022-10-28 14:56:39 +0530

    Create Next Greater Element.java

---
+++ STACKS AND QUEUES/Implementation/Next Greater Element.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int[] arr1 = new int[n1];
+		for(int i=0;i<n1;i++)
+		{
+		    arr1[i] = sc.nextInt();
+		}
+		int n2 = sc.nextInt();
+		int[] arr2 = new int[n2];
+		for(int i=0;i<n2;i++)
+		{
+		    arr2[i] = sc.nextInt();
+		}
+		Stack<Integer> s = new Stack<>();
+		HashMap<Integer,Integer> map = new HashMap<>();
+		for(int i:arr2)
+		{
+		    while(!s.empty() && i<s.peek())
+		    {
+		        map.put(s.pop(),i);
+		    }
+		    s.push(i);
+		}
+		int[] res = new int[n1];
+		int j=0;
+		for(int i=0;i<n1;i++)
+		{
+		    res[j++] = map.getOrDefault(arr1[i],-1);
+		}
+		for(int num:res)
+		{
+		    System.out.print(num+" ");
+		}
+	}
+}
